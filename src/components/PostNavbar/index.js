fefb2f7d5c93a57cceb9bb2685990727e685bee4
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-20 17:58:28 +0300

    add likes functionality

--- src/components/PostNavbar/index.js
+++ src/components/PostNavbar/index.js
@@ -1,30 +1,68 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
-import { BsHeart, BsCursor, BsChat, BsBookmark } from "react-icons/bs";
+import {
+  BsHeart,
+  BsFillHeartFill,
+  BsCursor,
+  BsChat,
+  BsBookmark,
+} from "react-icons/bs";
+import { projectFirestore } from "../../firebase/config";
+import { GlobalStateContext } from "../../context";
 
-export default function PostNavbar() {
+export default function PostNavbar({ postId }) {
+  const { username } = useContext(GlobalStateContext);
+  const [active, setActive] = useState(false);
+  const likePost = () => {
+    setActive(true);
+    projectFirestore
+      .collection("posts")
+      .doc(postId)
+      .get()
+      .then((result) => {
+        const likes = result.data().likes;
+        if (likes.includes(username)) {
+          console.log("You have already liked that post!");
+          return;
+        }
+        likes.push(username);
+        projectFirestore
+          .collection("posts")
+          .doc(postId)
+          .update({ likes })
+          .then(() => {
+            console.log("Liked Successfully!");
+          })
+          .catch(console.error);
+      })
+      .catch(console.error);
+  };
+
   return (
-    <Navbar className="posts-navbar">
-      <Nav>
-        <Nav.Link href="#like" className="nav-icon">
-          <BsHeart />
-        </Nav.Link>
-      </Nav>
-      <Nav>
-        <Nav.Link href="#details" className="nav-icon">
-          <BsChat />
-        </Nav.Link>
-      </Nav>
-      <Nav>
-        <Nav.Link href="#share" className="nav-icon">
-          <BsCursor />
-        </Nav.Link>
-      </Nav>
-      <Nav>
-        <Nav.Link href="#bookmark" className="nav-icon">
-          <BsBookmark />
-        </Nav.Link>
-      </Nav>
-    </Navbar>
+    <>
+      <Navbar className="posts-navbar">
+        <Nav>
+          <Nav.Link href="#like" className="nav-icon" onClick={likePost}>
+            {active ? <BsFillHeartFill /> : <BsHeart />}
+          </Nav.Link>
+        </Nav>
+        <Nav>
+          <Link to="/post-comments-details/:id" className="nav-icon">
+            <BsChat />
+          </Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#share" className="nav-icon">
+            <BsCursor />
+          </Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#bookmark" className="nav-icon">
+            <BsBookmark />
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+    </>
   );
 }
