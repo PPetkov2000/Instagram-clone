ef823c946234f3d53245b455c81f1f4350af0bd6
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-08-29 17:32:54 +0300

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -9,9 +9,17 @@ import {
   ListGroup,
 } from "react-bootstrap";
 import { AiFillHome } from "react-icons/ai";
-import { BsCursor, BsHeart, BsCompass } from "react-icons/bs";
+import {
+  BsCursor,
+  BsHeart,
+  BsCompass,
+  BsPeopleCircle,
+  BsBookmark,
+  BsGearWide,
+} from "react-icons/bs";
 import { Link, useHistory } from "react-router-dom";
 import NotificationsItem from "../NotificationsItem";
+import { projectAuth } from "../../firebase/config";
 
 const NavBar = () => {
   const [notifications, setNotifications] = useState([
@@ -20,6 +28,11 @@ const NavBar = () => {
     { id: 3, user: "new_guy", userImageUrl: "/images/user_icon.png" },
   ]);
   const history = useHistory();
+
+  const logoutUser = () => {
+    projectAuth.signOut();
+    history.push("/login");
+  };
 
   return (
     <Navbar bg="light" variant="light" className="navbar">
@@ -82,6 +95,46 @@ const NavBar = () => {
             }
           >
             <BsHeart />
+          </OverlayTrigger>
+        </Nav.Link>
+        <Nav.Link href="#profile" className="nav-icon">
+          <OverlayTrigger
+            trigger="click"
+            key="bottom"
+            placement="bottom"
+            overlay={
+              <Popover className="navbar-profile-popover">
+                <Popover.Content>
+                  <p>
+                    <BsPeopleCircle className="navbar-profile-popover-icon" />
+                    <Link to="/profile" className="text-dark">
+                      Profile
+                    </Link>
+                  </p>
+                  <p>
+                    <BsBookmark className="navbar-profile-popover-icon" />
+                    <Link to="/saved" className="text-dark">
+                      Saved
+                    </Link>
+                  </p>
+                  <p>
+                    <BsGearWide className="navbar-profile-popover-icon" />
+                    <Link to="/settings" className="text-dark">
+                      Settings
+                    </Link>
+                  </p>
+                  <p style={{ cursor: "pointer" }} onClick={logoutUser}>
+                    Logout
+                  </p>
+                </Popover.Content>
+              </Popover>
+            }
+          >
+            <img
+              src="/images/user_icon.png"
+              alt="profile"
+              className="navbar-profile-img"
+            />
           </OverlayTrigger>
         </Nav.Link>
       </Nav>
