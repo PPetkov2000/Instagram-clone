fed09dd54e0aa574a37ba94e53003e4466f5ea58
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-09-02 22:39:44 +0300

    add follow and unfollow user functionality

--- src/components/PostCommentsDetails/index.js
+++ src/components/PostCommentsDetails/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
 import PostNavbar from "../PostNavbar";
@@ -8,15 +8,69 @@ import { GlobalStateContext } from "../../context";
 import { projectFirestore } from "../../firebase/config";
 
 const PostCommentsDetails = (props) => {
-  const context = useContext(GlobalStateContext);
   const postId = props.match.params.id;
   const [imageUrl, setImageUrl] = useState("");
+  const [likes, setLikes] = useState([]);
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [username, setUsername] = useState("");
+  const [creator, setCreator] = useState("");
+  const context = useContext(GlobalStateContext);
+  const uid = context && context.uid;
+  const profileImage = context && context.profileImage;
 
-  projectFirestore
-    .collection("posts")
-    .doc(postId)
-    .get()
-    .then((post) => setImageUrl(post.data().imageUrl));
+  useEffect(() => {
+    const unsub = projectFirestore
+      .collection("posts")
+      .doc(postId)
+      .onSnapshot((res) => {
+        setLikes(res.data().likes);
+        setImageUrl(res.data().imageUrl);
+        setUsername(res.data().username);
+        setCreator(res.data().creator);
+      });
+
+    return () => unsub();
+  }, [postId]);
+
+  useEffect(() => {
+    const unsub = projectFirestore
+      .collection("instagramUsers")
+      .doc(uid)
+      .onSnapshot((snapshot) => {
+        const following = snapshot.data().following;
+        const followers = snapshot.data().followers;
+
+        if (following.includes(creator)) {
+          setIsFollowing(true);
+        } else {
+          setIsFollowing(false);
+        }
+      });
+
+    return () => unsub();
+  }, [uid, creator]);
+
+  const followAndUnfollowUser = () => {
+    projectFirestore
+      .collection("instagramUsers")
+      .doc(uid)
+      .get()
+      .then((res) => {
+        let following = res.data().following;
+        let followers = res.data().followers;
+
+        if (!following.includes(creator)) {
+          following.push(creator);
+        } else {
+          following = following.filter((x) => x !== creator);
+        }
+
+        return projectFirestore
+          .collection("instagramUsers")
+          .doc(uid)
+          .update({ following });
+      });
+  };
 
   return (
     <div className="details-container">
@@ -24,23 +78,38 @@ const PostCommentsDetails = (props) => {
       <aside className="comments-section">
         <Card>
           <Card.Header className="post-details-header">
-            <Card.Link href="#profile">
+            <Link to="/profile">
               <Card.Img
                 variant="top"
-                src="/images/user_icon.png"
+                src={profileImage}
                 className="card-header-img"
               />
-            </Card.Link>
-            <strong>random_user:</strong> <Link to="#follow">Follow</Link>
+            </Link>
+            <strong>{username}:</strong>{" "}
+            {isFollowing ? (
+              <Card.Link href="#unfollow" onClick={followAndUnfollowUser}>
+                Unfollow
+              </Card.Link>
+            ) : (
+              <Card.Link href="#follow" onClick={followAndUnfollowUser}>
+                Follow
+              </Card.Link>
+            )}
           </Card.Header>
           <Card.Body>
             <Comments postId={postId} showAddComment={false} />
           </Card.Body>
           <Card.Footer className="post-details-footer">
             <PostNavbar postId={postId} />
-            <strong>1000 likes</strong>
+            {likes.length === 0 ? (
+              <strong>No likes</strong>
+            ) : likes.length === 1 ? (
+              <strong>{likes.length} like</strong>
+            ) : (
+              <strong>{likes.length} likes</strong>
+            )}
             <p className="text-muted">2 hours ago</p>
-            <AddComment postId={postId} username={context.username} />
+            <AddComment postId={postId} username={username} />
           </Card.Footer>
         </Card>
       </aside>
