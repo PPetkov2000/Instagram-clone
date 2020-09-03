6e504d1aa8403fa59995cc7144655c2890c97e05
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-09-03 22:22:54 +0300

    Update index.js

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,19 +1,18 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import ProfileHeader from "../ProfileHeader";
 import ProfileNavbar from "../ProfileNavbar";
 import ProfileActivities from "../ProfileActivities";
-import { GlobalStateContext } from "../../context";
 
-const Profile = () => {
+const Profile = (props) => {
+  const userId = props.match.params.id;
   const [openPosts, setOpenPosts] = useState(true);
   const [openChannel, setOpenChannel] = useState(false);
   const [openSaved, setOpenSaved] = useState(false);
   const [openTagged, setOpenTagged] = useState(false);
-  const context = useContext(GlobalStateContext);
 
   return (
     <div className="user-profile-container">
-      {context && <ProfileHeader userId={context.uid} />}
+      <ProfileHeader userId={userId} />
       <ProfileNavbar
         openPosts={openPosts}
         openChannel={openChannel}
