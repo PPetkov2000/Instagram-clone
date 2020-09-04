5850bf3d6bcc701e9e4d7dafb8587cd88feb40b5
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-09-04 23:34:04 +0300

    Update index.js

--- src/components/ProfileActivities/index.js
+++ src/components/ProfileActivities/index.js
@@ -1,20 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProfilePosts from "../ProfilePosts";
 import ProfileChannel from "../ProfileChannel";
 import ProfileSaved from "../ProfileSaved";
 import ProfileTagged from "../ProfileTagged";
+import { projectFirestore } from "../../firebase/config";
 
 const ProfileActivities = ({
   openPosts,
   openChannel,
   openSaved,
   openTagged,
+  userId,
 }) => {
+  const [posts, setPosts] = useState([]);
+  const [saved, setSaved] = useState([]);
+  const [savedPosts, setSavedPosts] = useState([]);
+
+  useEffect(() => {
+    const unsub = projectFirestore
+      .collection("posts")
+      .where("creator", "==", userId)
+      .onSnapshot((snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }))
+        );
+      });
+
+    return () => unsub();
+  }, [userId]);
+
+  useEffect(() => {
+    const unsub = projectFirestore
+      .collection("instagramUsers")
+      .doc(userId)
+      .onSnapshot((snapshot) => {
+        setSaved(snapshot.data().saved);
+      });
+
+    return () => unsub();
+  }, [userId]);
+
+  useEffect(() => {
+    setSavedPosts(
+      saved.reduce((result, postId) => {
+        projectFirestore
+          .collection("posts")
+          .doc(postId)
+          .onSnapshot((snapshot) => {
+            result.push({ id: snapshot.id, ...snapshot.data() });
+          });
+
+        return result;
+      }, [])
+    );
+  }, [saved]);
+
   return (
     <div className="user-profile-activities">
-      {openPosts && <ProfilePosts />}
+      {openPosts && <ProfilePosts posts={posts} />}
       {openChannel && <ProfileChannel />}
-      {openSaved && <ProfileSaved />}
+      {openSaved && <ProfileSaved savedPosts={savedPosts} />}
       {openTagged && <ProfileTagged />}
     </div>
   );
