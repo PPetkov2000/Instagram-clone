fa74dd8b3edb8a6b7477e0989181029963ae2a20
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-09-11 21:48:55 +0300

    Update context.js

--- src/utils/context.js
+++ src/utils/context.js
@@ -1,1 +1,29 @@
-context;
+import React, { useState, useEffect } from "react";
+import { projectAuth } from "../firebase/config";
+import { generateUserDocument } from "../firebase/user";
+
+const Context = ({ children }) => {
+  const [userObj, setUserObj] = useState({
+    user: null,
+  });
+
+  useEffect(() => {
+    const unsub = projectAuth.onAuthStateChanged(async (userAuth) => {
+      const user = await generateUserDocument(userAuth);
+      setUserObj({ user });
+    });
+
+    return () => unsub();
+  }, []);
+
+  return (
+    <GlobalStateContext.Provider value={userObj.user}>
+      {children}
+    </GlobalStateContext.Provider>
+  );
+};
+
+export const GlobalStateContext = React.createContext({
+  user: null,
+});
+export default Context;
