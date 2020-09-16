c1c8694c00bf8322f4477f581dee3fc3625147f4
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-09-16 20:44:31 +0300

    Add files via upload

---
+++ src/components/ProfileHeaderRestrictUserModal/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Modal, ListGroup } from "react-bootstrap";
+
+function ProfileHeaderRestrictUserModal({ showModal, hideModal }) {
+  return (
+    <Modal show={showModal} onHide={hideModal}>
+      <Modal.Body className="profile-header-restrict-user-body">
+        <ListGroup variant="flush">
+          <ListGroup.Item action className="text-danger">
+            <strong>Block this user</strong>
+          </ListGroup.Item>
+          <ListGroup.Item action className="text-danger">
+            <strong>Restrict</strong>
+          </ListGroup.Item>
+          <ListGroup.Item action className="text-danger">
+            <strong>Report User</strong>
+          </ListGroup.Item>
+          <ListGroup.Item action onClick={hideModal}>
+            Cancel
+          </ListGroup.Item>
+        </ListGroup>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default ProfileHeaderRestrictUserModal;
