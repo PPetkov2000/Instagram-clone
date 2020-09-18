5c448c08bf44f9239f76b04789faea40011b2aa7
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-09-19 00:07:58 +0300

    Update index.js

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const Comment = ({ comment }) => {
+  const history = useHistory();
+
+  const goToUserProfile = () => {
+    history.push(`/profile/${comment.creatorId}`);
+  };
+
   return (
     <p>
-      <strong>{comment.creator}</strong> {comment.text}
+      <strong onClick={goToUserProfile} style={{ cursor: "pointer" }}>
+        {comment.creatorUsername}
+      </strong>{" "}
+      {comment.text}
     </p>
   );
-}
+};
 
 export default Comment;
