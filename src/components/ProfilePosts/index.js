d1b79304ac2b1358f2099d04509c9eb33265d231
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-09-20 19:33:24 +0300

    Update index.js

--- src/components/ProfilePosts/index.js
+++ src/components/ProfilePosts/index.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsFillHeartFill, BsFillChatFill } from "react-icons/bs";
+import { Modal } from "react-bootstrap";
+import PostCommentsDetails from "../PostCommentsDetails";
 
 const ProfilePosts = ({ posts }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [postId, setPostId] = useState();
+  console.log(posts);
+
+  const openPost = (e) => {
+    setPostId(e.target.dataset.id);
+    setShowModal(true);
+  };
+
   return posts.length === 0 ? (
     <>
       <img src="/images/profile_image.jpg" alt="profile" />
@@ -22,10 +34,33 @@ const ProfilePosts = ({ posts }) => {
       {posts.map((post) => {
         return (
           <div key={post.id} className="profile-posts-img-div">
-            <img src={post.imageUrl} alt={post} className="profile-posts-img" />
+            <img src={post.imageUrl} alt="post" className="profile-posts-img" />
+            <div
+              className="profile-posts-hover"
+              data-id={post.id}
+              onClick={openPost}
+            >
+              <span>
+                <BsFillHeartFill /> {post.likes.length}
+              </span>
+              <span>
+                <BsFillChatFill /> {post.comments.length}
+              </span>
+            </div>
           </div>
         );
       })}
+
+      <Modal
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        centered
+        className="profile-posts-modal"
+      >
+        <Modal.Body className="profile-posts-modal-body">
+          <PostCommentsDetails postId={postId} />
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
