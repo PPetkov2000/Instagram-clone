74f41b7bfcc040d78714d0002d42fc818cf5352b
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-09-22 16:58:22 +0300

    add changepassword page

---
+++ src/components/ProfileChangePassword/index.js
@@ -0,0 +1,83 @@
+import React, { useState, useContext } from "react";
+import { Form, Button } from "react-bootstrap";
+import { GlobalStateContext } from "../../utils/context";
+import ProfileSettingsFormGroup from "../ProfileSettingsFormGroup";
+import { projectAuth } from "../../firebase/config";
+
+function ProfileChangePassword() {
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const context = useContext(GlobalStateContext);
+
+  const handleChangePassword = (e) => {
+    e.preventDefault();
+    console.log(`Old Password: ${oldPassword}`);
+    console.log(`New Password: ${newPassword}`);
+    console.log(`Confirm Password: ${confirmPassword}`);
+
+    const user = projectAuth.currentUser;
+
+    user
+      .updatePassword(newPassword)
+      .then(() => {
+        // Update successful.
+      })
+      .catch(console.error);
+  };
+
+  return (
+    <>
+      <div className="change-password-tab-pane-header">
+        <div className="change-password-img-container">
+          <img
+            src={context && context.profileImage}
+            alt="change-password"
+            className="change-password-img"
+          />
+        </div>
+        <div className="change-password-tab-pane-text">
+          <h4 className="change-password-tab-pane-title">
+            {context && context.username}
+          </h4>
+        </div>
+      </div>
+      <Form onSubmit={handleChangePassword}>
+        <ProfileSettingsFormGroup
+          labelText="Old Password"
+          fieldType="password"
+          handleChange={(e) => setOldPassword(e.target.value)}
+        />
+        <ProfileSettingsFormGroup
+          labelText="New Password"
+          fieldType="password"
+          handleChange={(e) => setNewPassword(e.target.value)}
+        />
+        <ProfileSettingsFormGroup
+          labelText="Confirm New Password"
+          fieldType="password"
+          handleChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <div className="change-password-buttons-container">
+          <aside style={{ flex: "0.3" }}></aside>
+          <div className="change-password-buttons">
+            <Button
+              type="submit"
+              disabled={!oldPassword || !newPassword || !confirmPassword}
+              style={{ fontSize: "0.9rem" }}
+            >
+              Change Password
+            </Button>
+            <div>
+              <a href="#forgot-password" style={{ textDecoration: "none" }}>
+                Forgot password?
+              </a>
+            </div>
+          </div>
+        </div>
+      </Form>
+    </>
+  );
+}
+
+export default ProfileChangePassword;
