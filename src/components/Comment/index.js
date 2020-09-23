348bd91d4977c0ee65378b60e273e1a06e58c2cd
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-09-24 00:06:25 +0300

    Update index.js

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,20 +1,148 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import { BsHeart } from "react-icons/bs";
+import { FcLike } from "react-icons/fc";
+import { projectFirestore } from "../../firebase/config";
+import { useGlobalContext } from "../../utils/context";
+import requester from "../../firebase/requester";
+import formatTimestamp from "../../utils/formatTimestamp";
+import CommentLikesModal from "../CommentLikesModal";
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, post }) => {
+  const [liked, setLiked] = useState(false);
+  const [showLikesModal, setShowLikesModal] = useState(false);
+  const [likedCommentUsers, setLikedCommentUsers] = useState([]);
+  const [currentUserFollowing, setCurrentUserFollowing] = useState([]);
   const history = useHistory();
+  const context = useGlobalContext();
+  const uid = context && context.uid;
+
+  useEffect(() => {
+    const unsub = projectFirestore
+      .collection("posts")
+      .doc(comment.postId)
+      .collection("comments")
+      .doc(comment.id)
+      .onSnapshot((snapshot) => {
+        setLiked(snapshot.data().likes.includes(uid));
+      });
+
+    return () => unsub();
+  }, [comment.postId, comment.id, uid]);
+
+  useEffect(() => {
+    comment.likes.forEach((id) => {
+      requester
+        .get("instagramUsers", id)
+        .then((res) => {
+          setLikedCommentUsers((prevLikes) => {
+            return [...prevLikes, { id: res.id, ...res.data() }];
+          });
+        })
+        .catch(console.error);
+    });
+  }, [comment.likes]);
+
+  useEffect(() => {
+    const unsub = projectFirestore
+      .collection("instagramUsers")
+      .doc(uid)
+      .onSnapshot((snapshot) => {
+        setCurrentUserFollowing((prevFollowing) => {
+          return [...prevFollowing, snapshot.data().following];
+        });
+      });
+
+    return () => unsub();
+  }, [uid]);
 
   const goToUserProfile = () => {
     history.push(`/profile/${comment.creatorId}`);
   };
 
+  const openLikesModal = () => setShowLikesModal(true);
+  const closeLikesModal = () => setShowLikesModal(false);
+
+  const likeAndDislikeComment = () => {
+    Promise.all([
+      projectFirestore
+        .collection("posts")
+        .doc(comment.postId)
+        .collection("comments")
+        .doc(comment.id)
+        .get(),
+      requester.get("instagramUsers", comment.creatorId),
+    ])
+      .then(([currentComment, commentCreator]) => {
+        let currentCommentLikes = currentComment.data().likes;
+        let commentCreatorNotifications = commentCreator.data().notifications;
+
+        if (!currentCommentLikes.includes(uid)) {
+          currentCommentLikes.push(uid);
+          commentCreatorNotifications.push({
+            id: uid,
+            username: context && context.username,
+            profileImage: context && context.profileImage,
+            timestamp: new Date(),
+            type: "like-comment",
+            postId: comment.postId,
+            postImageUrl: post.imageUrl,
+          });
+        } else {
+          currentCommentLikes = currentCommentLikes.filter((x) => x !== uid);
+          commentCreatorNotifications = commentCreatorNotifications.filter(
+            (x) => x.id !== uid
+          );
+        }
+
+        return Promise.all([
+          projectFirestore
+            .collection("posts")
+            .doc(comment.postId)
+            .collection("comments")
+            .doc(comment.id)
+            .update({ likes: currentCommentLikes }),
+          requester.update("instagramUsers", comment.creatorId, {
+            notifications: commentCreatorNotifications,
+          }),
+        ]);
+      })
+      .catch(console.error);
+  };
+
   return (
-    <p>
-      <strong onClick={goToUserProfile} style={{ cursor: "pointer" }}>
-        {comment.creatorUsername}
-      </strong>{" "}
-      {comment.text}
-    </p>
+    <>
+      <div className="comment-container">
+        <p>
+          <strong onClick={goToUserProfile} style={{ cursor: "pointer" }}>
+            {comment.creatorUsername}
+          </strong>{" "}
+          {comment.text}
+        </p>
+        {liked ? (
+          <FcLike className="comment-icon" onClick={likeAndDislikeComment} />
+        ) : (
+          <BsHeart className="comment-icon" onClick={likeAndDislikeComment} />
+        )}
+      </div>
+      <div className="comment-details">
+        <p className="text-muted">{formatTimestamp(comment.timestamp)}</p>
+        {comment.likes.length > 0 && (
+          <p className="text-muted" onClick={openLikesModal}>
+            {comment.likes.length}{" "}
+            {comment.likes.length === 1 ? "like" : "likes"}
+          </p>
+        )}
+        <p className="text-muted">Reply</p>
+      </div>
+
+      <CommentLikesModal
+        showModal={showLikesModal}
+        hideModal={closeLikesModal}
+        likedCommentUsers={likedCommentUsers}
+        currentUserFollowing={currentUserFollowing}
+      />
+    </>
   );
 };
 
