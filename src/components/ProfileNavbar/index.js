760fdff31884e55f9c3f1e644472ce78f58c48fd
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-09-29 22:50:14 +0300

    Update index.js

--- src/components/ProfileNavbar/index.js
+++ src/components/ProfileNavbar/index.js
@@ -1,141 +1,82 @@
-import React, { useState, useContext, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { Navbar, Nav } from "react-bootstrap";
-import {
-  BsHeart,
-  BsCursor,
-  BsChat,
-  BsBookmark,
-  BsFillBookmarkFill,
-} from "react-icons/bs";
-import { FcLike } from "react-icons/fc";
-import { projectFirestore } from "../../firebase/config";
-import { GlobalStateContext } from "../../utils/context";
-import PostNavbarModal from "../PostNavbarModal";
-import requester from "../../firebase/requester";
+import React from "react";
+import { BsGrid3X3, BsBookmark, BsPersonSquare, BsTv } from "react-icons/bs";
+import { useGlobalContext } from "../../utils/context";
 
-const PostNavbar = ({ post }) => {
-  const [liked, setLiked] = useState(false);
-  const [saved, setSaved] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const history = useHistory();
-  const context = useContext(GlobalStateContext);
+const ProfileNavbar = ({
+  openPosts,
+  openChannel,
+  openSaved,
+  openTagged,
+  setOpenPosts,
+  setOpenChannel,
+  setOpenSaved,
+  setOpenTagged,
+  userId,
+}) => {
+  const context = useGlobalContext();
   const uid = context && context.uid;
 
-  useEffect(() => {
-    const unsub = projectFirestore
-      .collection("posts")
-      .doc(post.id)
-      .onSnapshot((snapshot) => {
-        setLiked(snapshot.data().likes.includes(uid));
-      });
-
-    return () => unsub();
-  }, [post.id, uid]);
-
-  useEffect(() => {
-    const unsub = projectFirestore
-      .collection("instagramUsers")
-      .doc(uid)
-      .onSnapshot((snapshot) => {
-        setSaved(snapshot.data().saved.includes(post.id));
-      });
-
-    return () => unsub();
-  }, [uid, post.id]);
-
-  const likeAndDislikePost = () => {
-    Promise.all([
-      requester.get("posts", post.id),
-      requester.get("instagramUsers", post.creator),
-    ])
-      .then(([currentPost, currentUser]) => {
-        let currentPostLikes = currentPost.data().likes;
-        let currentUserNotifications = currentUser.data().notifications;
-
-        if (!currentPostLikes.includes(uid)) {
-          currentPostLikes.push(uid);
-          currentUserNotifications.push({
-            id: uid,
-            username: context.username,
-            profileImage: context.profileImage,
-            timestamp: new Date(),
-            type: "like",
-            postId: post.id,
-            postImageUrl: post.imageUrl,
-          });
-        } else {
-          currentPostLikes = currentPostLikes.filter((x) => x !== uid);
-          currentUserNotifications = currentUserNotifications.filter(
-            (x) => x.id !== uid
-          );
-        }
-
-        return Promise.all([
-          requester.update("posts", post.id, { likes: currentPostLikes }),
-          requester.update("instagramUsers", post.creator, {
-            notifications: currentUserNotifications,
-          }),
-        ]);
-      })
-      .catch(console.error);
+  const openProfilePosts = () => {
+    setOpenPosts(true);
+    setOpenChannel(false);
+    setOpenSaved(false);
+    setOpenTagged(false);
   };
-
-  const saveAndUnSavePost = () => {
-    requester
-      .get("instagramUsers", uid)
-      .then((res) => {
-        let saved = res.data().saved;
-
-        if (!saved.includes(post.id)) {
-          saved.push(post.id);
-        } else {
-          saved = saved.filter((x) => x !== post.id);
-        }
-
-        return requester.update("instagramUsers", uid, { saved });
-      })
-      .catch(console.error);
+  const openProfileChannel = () => {
+    setOpenPosts(false);
+    setOpenChannel(true);
+    setOpenSaved(false);
+    setOpenTagged(false);
   };
-
-  const showOptions = () => setShowModal(true);
-  const hideOptions = () => setShowModal(false);
-
-  const openPostDetails = () => {
-    history.push(`/post-comments-details/${post.id}`);
+  const openProfileSaved = () => {
+    setOpenPosts(false);
+    setOpenChannel(false);
+    setOpenSaved(true);
+    setOpenTagged(false);
+  };
+  const openProfileTagged = () => {
+    setOpenPosts(false);
+    setOpenChannel(false);
+    setOpenSaved(false);
+    setOpenTagged(true);
   };
 
   return (
-    <>
-      <Navbar className="posts-navbar">
-        <Nav>
-          <Nav.Link
-            href="#like"
-            className="nav-icon"
-            onClick={likeAndDislikePost}
+    <nav className="user-profile-navbar">
+      <a
+        href="#posts"
+        className={openPosts ? "profile-navbar-active" : ""}
+        onClick={openProfilePosts}
+      >
+        <BsGrid3X3 className="user-profile-navbar-icon" /> posts
+      </a>
+      {userId === uid && (
+        <>
+          <a
+            href="#channel"
+            className={openChannel ? "profile-navbar-active" : ""}
+            onClick={openProfileChannel}
           >
-            {liked ? <FcLike /> : <BsHeart />}
-          </Nav.Link>
-
-          <Nav.Link className="nav-icon" onClick={openPostDetails}>
-            <BsChat />
-          </Nav.Link>
-          <Nav.Link href="#options" className="nav-icon" onClick={showOptions}>
-            <BsCursor />
-          </Nav.Link>
-          <Nav.Link
-            href="#bookmark"
-            className="nav-icon posts-navbar-nav"
-            onClick={saveAndUnSavePost}
+            <BsTv className="user-profile-navbar-icon" /> igtv
+          </a>
+          <a
+            href="#saved"
+            className={openSaved ? "profile-navbar-active" : ""}
+            onClick={openProfileSaved}
           >
-            {saved ? <BsFillBookmarkFill /> : <BsBookmark />}
-          </Nav.Link>
-        </Nav>
-      </Navbar>
-
-      <PostNavbarModal showModal={showModal} hideOptions={hideOptions} />
-    </>
+            <BsBookmark className="user-profile-navbar-icon" /> saved
+          </a>
+        </>
+      )}
+      <a
+        href="#tagged"
+        className={openTagged ? "profile-navbar-active" : ""}
+        onClick={openProfileTagged}
+      >
+        <BsPersonSquare className="user-profile-navbar-icon" /> tagged
+      </a>
+    </nav>
   );
 };
 
-export default PostNavbar;
+export default ProfileNavbar;
