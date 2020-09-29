10daedbc4bd86818d130c80e591f163939c02bd8
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-09-30 00:40:41 +0300

    add like comment action

--- src/utils/formatNotification.js
+++ src/utils/formatNotification.js
@@ -5,6 +5,7 @@ export default function (notification) {
     follower: (username) => `${username} followed you`,
     like: (username) => `${username} liked your post`,
     comment: (username) => `${username} commented on your post`,
+    "like-comment": (username) => `${username} liked your comment`,
   };
 
   if (typeof notificationTypes[type] === "function") {
