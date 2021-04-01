7125f6159985fe340542993a521383c82297d9b4
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-04-02 02:21:56 +0300

    Update index.js

--- src/components/Profile/ProfileHeaderUserStatusModal/index.js
+++ src/components/Profile/ProfileHeaderUserStatusModal/index.js
@@ -1,36 +1,36 @@
 import React from "react";
 import { Modal, ListGroup } from "react-bootstrap";
 import requester from "../../../firebase/requester";
+import { useGlobalContext } from "../../../utils/context";
 
-function ProfileHeaderUserStatusModal({
-  showModal,
-  hideModal,
-  userId,
-  uid,
-  userProfileImage,
-  username,
-}) {
+function ProfileHeaderUserStatusModal({ showModal, hideModal, clickedUser }) {
+  const authUser = useGlobalContext();
+
   const unfollowUser = () => {
     Promise.all([
-      requester.get("instagramUsers", uid),
-      requester.get("instagramUsers", userId),
+      requester.get("instagramUsers", authUser.uid),
+      requester.get("instagramUsers", clickedUser.id),
     ])
       .then(([currentUser, unfollowedUser]) => {
         let currentUserFollowing = currentUser.data().following;
         let unfollowedUserFollowers = unfollowedUser.data().followers;
         let unfollowedUserNotifications = unfollowedUser.data().notifications;
 
-        currentUserFollowing = currentUserFollowing.filter((x) => x !== userId);
-        unfollowedUserFollowers = currentUserFollowing.filter((x) => x !== uid);
+        currentUserFollowing = currentUserFollowing.filter(
+          (x) => x !== clickedUser.id
+        );
+        unfollowedUserFollowers = currentUserFollowing.filter(
+          (x) => x !== authUser.uid
+        );
         unfollowedUserNotifications = unfollowedUserNotifications.filter(
-          (x) => x.id !== uid
+          (x) => x.id !== authUser.uid
         );
 
         return Promise.all([
-          requester.update("instagramUsers", uid, {
+          requester.update("instagramUsers", authUser.uid, {
             following: currentUserFollowing,
           }),
-          requester.update("instagramUsers", userId, {
+          requester.update("instagramUsers", clickedUser.id, {
             followers: unfollowedUserFollowers,
             notifications: unfollowedUserNotifications,
           }),
@@ -45,13 +45,13 @@ function ProfileHeaderUserStatusModal({
     <Modal show={showModal} onHide={hideModal} centered>
       <Modal.Header className="profile-header-user-status-header">
         <img
-          src={userProfileImage}
+          src={clickedUser?.profileImage}
           alt="modal"
           className="profile-header-user-status-img"
         />
         <p>
-          If you change your mind, you'll have to request to follow @{username}{" "}
-          again.
+          If you change your mind, you'll have to request to follow @
+          {clickedUser?.username} again.
         </p>
       </Modal.Header>
       <Modal.Body className="profile-header-user-status-body">
