8472fa910e2e9563bbd5433f2f07099af4d2ff7d
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-04-02 18:30:59 +0300

    Update index.js

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,18 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ProfileHeader from "../../components/Profile/ProfileHeader";
 import ProfileNavbar from "../../components/Profile/ProfileNavbar";
 import ProfileActivities from "../../components/Profile/ProfileActivities";
+import { projectFirestore } from "../../firebase/config";
+import requester from "../../firebase/requester";
 
-const Profile = (props) => {
-  const userId = props.match.params.id;
+const Profile = ({ match }) => {
+  const userId = match.params.id;
   const [openPosts, setOpenPosts] = useState(true);
   const [openChannel, setOpenChannel] = useState(false);
   const [openSaved, setOpenSaved] = useState(false);
   const [openTagged, setOpenTagged] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
+  const [currentUserPosts, setCurrentUserPosts] = useState([]);
+
+  useEffect(() => {
+    if (!userId) return;
+
+    const unsub = projectFirestore
+      .collection("instagramUsers")
+      .doc(userId)
+      .onSnapshot((snapshot) => {
+        setCurrentUser({ id: snapshot.id, ...snapshot.data() });
+      });
+
+    return () => unsub();
+  }, [userId]);
+
+  useEffect(() => {
+    const unsub = requester
+      .getByCriteria("posts", "creator", userId)
+      .then((res) => {
+        setCurrentUserPosts(
+          res.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        );
+      });
+
+    return () => unsub;
+  }, [userId]);
 
   return (
     <div className="user-profile-container">
-      <ProfileHeader userId={userId} />
+      {currentUser && (
+        <ProfileHeader
+          currentUser={currentUser}
+          currentUserPosts={currentUserPosts}
+        />
+      )}
       <ProfileNavbar
         openPosts={openPosts}
         openChannel={openChannel}
