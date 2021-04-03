8273b712c83a86ddd4af82f51145c229b88b4d2f
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-04-04 00:21:25 +0300

    add user actions helpers

---
+++ src/utils/userActions.js
@@ -0,0 +1,91 @@
+import requester from "../firebase/requester";
+
+const followAndUnfollowUser = (authUser, followedUserId) => {
+  requester
+    .get("instagramUsers", followedUserId)
+    .then((followedUser) => {
+      let followedUserFollowers = followedUser.data().followers;
+      let followedUserNotifications = followedUser.data().notifications;
+
+      if (!authUser.following.includes(followedUserId)) {
+        authUser.following.push(followedUserId);
+        followedUserFollowers.push(authUser.uid);
+        followedUserNotifications.push({
+          id: authUser.uid,
+          username: authUser.username,
+          profileImage: authUser.profileImage,
+          timestamp: new Date(),
+          type: "follower",
+        });
+      } else {
+        authUser.following = authUser.following.filter(
+          (x) => x !== followedUserId
+        );
+        followedUserFollowers = followedUserFollowers.filter(
+          (x) => x !== authUser.uid
+        );
+        followedUserNotifications = followedUserNotifications.filter(
+          (x) => x.id !== authUser.uid
+        );
+      }
+
+      return Promise.all([
+        requester.update("instagramUsers", authUser.uid, {
+          following: authUser.following,
+        }),
+        requester.update("instagramUsers", followedUserId, {
+          followers: followedUserFollowers,
+          notifications: followedUserNotifications,
+        }),
+      ]);
+    })
+    .catch(console.error);
+};
+
+const likeAndDislikePost = (authUser, likedPost) => {
+  requester
+    .get("instagramUsers", likedPost.creator)
+    .then((likedPostCreator) => {
+      let likedPostCreatorNotifications = likedPostCreator.data().notifications;
+
+      if (!likedPost.likes.includes(authUser.uid)) {
+        likedPost.likes.push(authUser.uid);
+        likedPostCreatorNotifications.push({
+          id: authUser.uid,
+          username: authUser.username,
+          profileImage: authUser.profileImage,
+          timestamp: new Date(),
+          type: "like",
+          postId: likedPost.id,
+          postImageUrl: likedPost.imageUrl,
+        });
+      } else {
+        likedPost.likes = likedPost.likes.filter((x) => x !== authUser.uid);
+        likedPostCreatorNotifications = likedPostCreatorNotifications.filter(
+          (x) => x.id !== authUser.uid
+        );
+      }
+
+      return Promise.all([
+        requester.update("posts", likedPost.id, { likes: likedPost.likes }),
+        requester.update("instagramUsers", likedPost.creator, {
+          notifications: likedPostCreatorNotifications,
+        }),
+      ]);
+    })
+    .catch(console.error);
+};
+
+const saveAndUnSavePost = (authUser, savedPostId) => {
+  if (!authUser.saved.includes(savedPostId)) {
+    authUser.saved.push(savedPostId);
+  } else {
+    authUser.saved = authUser.saved.filter((x) => x !== savedPostId);
+  }
+
+  return requester.update("instagramUsers", authUser.uid, {
+    saved: authUser.saved,
+  });
+};
+
+export { followAndUnfollowUser, likeAndDislikePost, saveAndUnSavePost };
