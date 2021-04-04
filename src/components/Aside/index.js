7cfac944e5be01b7e8c9f354514e28b31ec7224f
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-04-05 00:38:44 +0300

    Update index.js

--- src/components/Aside/index.js
+++ src/components/Aside/index.js
@@ -1,41 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Suggestions from "../Suggestions";
-import { useGlobalContext } from "../../utils/context";
-import { projectFirestore } from "../../firebase/config";
+import { useAuth } from "../../utils/authProvider";
 
 const Aside = () => {
-  const [profileImage, setProfileImage] = useState();
-  const context = useGlobalContext();
-  const uid = context && context.uid;
+  const authUser = useAuth();
 
-  useEffect(() => {
-    if (uid == null) return;
-
-    const unsub = projectFirestore
-      .collection("instagramUsers")
-      .doc(uid)
-      .onSnapshot((snapshot) => {
-        setProfileImage(snapshot.data().profileImage);
-      });
-
-    return () => unsub();
-  }, [uid]);
-
-  return (
+  return authUser ? (
     <aside className="aside">
       <div className="aside-profile">
-        <Link to={`/profile/${uid}`}>
+        <Link to={`/profile/${authUser.uid}`}>
           <img
-            src={profileImage}
+            src={authUser.profileImage}
             alt="user_icon"
             className="aside-profile-img"
           />
         </Link>
         <div className="aside-profile-content">
-          <strong>{context && context.username}</strong>
+          <strong>{authUser.username}</strong>
           <p className="text-muted" style={{ margin: "0" }}>
-            {context && context.fullName}
+            {authUser.fullName}
           </p>
         </div>
       </div>
@@ -54,6 +38,8 @@ const Aside = () => {
         <p className="text-muted">@Instagram 2020</p>
       </div>
     </aside>
+  ) : (
+    <h2>Loading...</h2>
   );
 };
 
