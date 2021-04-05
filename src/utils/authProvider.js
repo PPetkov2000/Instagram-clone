6a94ea971405eebc1d4bdcd067ea0459fd7c3fbb
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-04-06 00:13:27 +0300

    add auth user state

--- src/utils/authProvider.js
+++ src/utils/authProvider.js
@@ -5,18 +5,28 @@ import { generateUserDocument } from "../firebase/user";
 const AuthContext = React.createContext(null);
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [authState, setAuthState] = useState({
+    loading: true,
+    authUser: null,
+    isLoggedIn: false,
+  });
 
   useEffect(() => {
     const unsub = projectAuth.onAuthStateChanged(async (userAuth) => {
       const userInfo = await generateUserDocument(userAuth);
-      setUser(userInfo);
+      if (userAuth) {
+        setAuthState({ isLoggedIn: true, loading: false, authUser: userInfo });
+      } else {
+        setAuthState({ ...authState, loading: false, authUser: null });
+      }
     });
 
     return () => unsub();
-  }, []);
+  }, [authState]);
 
-  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={authState}>{children}</AuthContext.Provider>
+  );
 };
 
 export function useAuth() {
