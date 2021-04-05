55431e2a236b6df61d0e947693f75bd4a9001bf2
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-04-06 00:11:50 +0300

    Update index.js

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -39,6 +39,30 @@ const Profile = ({ match }) => {
     return () => unsub;
   }, [userId]);
 
+  const resetActions = () => {
+    setOpenPosts(false);
+    setOpenChannel(false);
+    setOpenSaved(false);
+    setOpenTagged(false);
+  };
+
+  const openPostsHandler = () => {
+    resetActions();
+    setOpenPosts(true);
+  };
+  const openChannelHandler = () => {
+    resetActions();
+    setOpenChannel(true);
+  };
+  const openSavedHandler = () => {
+    resetActions();
+    setOpenSaved(true);
+  };
+  const openTaggedHandler = () => {
+    resetActions();
+    setOpenTagged(true);
+  };
+
   return (
     <div className="user-profile-container">
       {currentUser && (
@@ -52,19 +76,22 @@ const Profile = ({ match }) => {
         openChannel={openChannel}
         openSaved={openSaved}
         openTagged={openTagged}
-        setOpenPosts={setOpenPosts}
-        setOpenChannel={setOpenChannel}
-        setOpenSaved={setOpenSaved}
-        setOpenTagged={setOpenTagged}
-        userId={userId}
+        openPostsHandler={openPostsHandler}
+        openChannelHandler={openChannelHandler}
+        openSavedHandler={openSavedHandler}
+        openTaggedHandler={openTaggedHandler}
+        currentUserId={currentUser.id}
       />
-      <ProfileActivities
-        openPosts={openPosts}
-        openChannel={openChannel}
-        openSaved={openSaved}
-        openTagged={openTagged}
-        userId={userId}
-      />
+      {currentUser && (
+        <ProfileActivities
+          openPosts={openPosts}
+          openChannel={openChannel}
+          openSaved={openSaved}
+          openTagged={openTagged}
+          currentUser={currentUser}
+          currentUserPosts={currentUserPosts}
+        />
+      )}
       <footer className="user-profile-footer">
         <h5>@Instagram 2020</h5>
       </footer>
