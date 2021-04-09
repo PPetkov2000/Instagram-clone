9a588213cce4581886a5b21e7b96721bdc5004b8
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-04-09 23:42:01 +0300

    refactor code

--- src/components/AddComment/index.js
+++ src/components/AddComment/index.js
@@ -1,55 +1,11 @@
 import React, { useState } from "react";
 import { InputGroup, FormControl, Button } from "react-bootstrap";
-import { projectFirestore, timestamp } from "../../firebase/config";
 import { useAuth } from "../../utils/authProvider";
-import requester from "../../firebase/requester";
+import { createComment } from "../../utils/userActions";
 
 const AddComment = ({ post }) => {
   const [comment, setComment] = useState("");
-  const authUser = useAuth();
-  const authUserId = authUser && authUser.uid;
-
-  const publishComment = () => {
-    projectFirestore
-      .collection("posts")
-      .doc(post.id)
-      .collection("comments")
-      .add({
-        text: comment,
-        creatorId: authUserId,
-        creatorUsername: authUser && authUser.username,
-        postId: post.id,
-        timestamp: timestamp(),
-        likes: [],
-      })
-      .then(() => {
-        if (authUserId === post.creator) return;
-
-        requester
-          .get("instagramUsers", post.creator)
-          .then((res) => {
-            const notifications = res.data().notifications;
-
-            notifications.push({
-              id: authUserId,
-              username: authUser && authUser.username,
-              profileImage: authUser && authUser.profileImage,
-              timestamp: new Date(),
-              type: "comment",
-              postId: post.id,
-              postImageUrl: post.imageUrl,
-            });
-
-            return requester.update("instagramUsers", post.creator, {
-              notifications,
-            });
-          })
-          .catch(console.error);
-      })
-      .catch(console.error);
-
-    setComment("");
-  };
+  const { authUser } = useAuth();
 
   return (
     <div className="add-comment mt-3">
@@ -57,7 +13,7 @@ const AddComment = ({ post }) => {
         <FormControl
           placeholder="Add comment..."
           aria-label="Add comment..."
-          aria-describedby="basic-addon2"
+          aria-describedby="Add comment..."
           value={comment}
           onChange={(e) => {
             setComment(e.target.value);
@@ -66,7 +22,10 @@ const AddComment = ({ post }) => {
         <InputGroup.Append>
           <Button
             variant="success"
-            onClick={publishComment}
+            onClick={() => {
+              createComment(authUser, post, comment);
+              setComment("");
+            }}
             disabled={!comment}
           >
             Publish
