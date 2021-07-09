6a4b549bf235affa45195dab1ea7de2f395303a5
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-07-10 01:40:12 +0300

    update auth provider path

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { BsFillTrashFill, BsHeart, BsHeartFill } from "react-icons/bs";
 import { projectFirestore } from "../../firebase/config";
-import { useAuth } from "../../utils/authProvider";
+import { useAuth } from "../../contexts/authProvider";
 import formatTimestamp from "../../utils/formatTimestamp";
 import CommentLikesModal from "../CommentLikesModal";
 import {
